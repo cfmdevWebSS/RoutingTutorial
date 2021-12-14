16f47572f4813896ab1a44e05d4ca468545152e4
Lawrence Buta <larry.buta@gmail.com>
2021-12-13 19:16:33 -0500

    Interim Save of React Routing Tutorial-12-13-2021

--- src/routes/invoices.js
+++ src/routes/invoices.js
@@ -1,9 +1,9 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useSearchParams } from "react-router-dom";
 import { getInvoices } from "../data";
-import Invoice from "./invoice";
 
 export default function Invoices() {
   let invoices = getInvoices();
+  let [searchParams, setSearchParams] = useSearchParams();
   return (
     <div style={{ display: "flex" }}>
       <nav
@@ -12,14 +12,37 @@ export default function Invoices() {
           padding: "1rem"
         }}
       >
-        {invoices.map(invoice => (
-          <Link
-            style={{ display: "block", margin: "1rem 0" }}
+        <input
+          value={searchParams.get("filter") || ""}
+          onChange={event => {
+            let filter = event.target.value;
+            if (filter) {
+              setSearchParams({ filter });
+            } else {
+              setSearchParams({});
+            }
+          }}
+        />
+        {invoices
+          .filter(invoice => {
+            let filter = searchParams.get("filter");
+            if (!filter) return true;
+            let name = invoice.name.toLowerCase();
+            return name.startsWith(filter.toLowerCase());
+          }).map(invoice => (
+          <NavLink
+            style={({ isActive }) => {
+              return {
+                display: "block",
+                margin: "1rem 0",
+                color: isActive ? "red" : "blue"
+              };
+            }}
             to={`/invoices/${invoice.number}`}
             key={invoice.number}
           >
             {invoice.name}
-          </Link>
+          </NavLink>
         ))}
       </nav>
       <Outlet />
